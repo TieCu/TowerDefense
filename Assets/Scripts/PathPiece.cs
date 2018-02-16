ce320f103c43af8d78f54591e66b8e8d486dcc4b
bdorman <bdorman@student.neumont.edu>
2018-02-16 15:01:31 -0700

    Started path script, tile is going to be used as a parent type with path inheriting

---
+++ Assets/Scripts/PathPiece.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PathPiece : MonoBehaviour
+{
+	enum eDirection { UP, DOWN, LEFT, RIGHT }
+	enum eFunction { PATH, SPAWN, FINISH }
+
+	[SerializeField] eDirection m_contribute;
+	[SerializeField] eFunction m_purpose;
+
+	void Start()
+	{
+
+	}
+
+	void Update()
+	{
+
+	}
+}
