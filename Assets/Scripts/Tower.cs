b752da609bb94bb826e7d03e9271759e46b1771e
Unholydeath <brbodily@student.neumont.edu>
2018-02-16 17:41:56 -0700

    Tower script skeleton

---
+++ Assets/Scripts/Tower.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Tower : MonoBehaviour
+{
+    [SerializeField] [Range(1.0f, 50.0f)] float m_attackRadius = 15.0f;
+    [SerializeField] [Range(1.0f, 1000.0f)] float m_damage = 10.0f;
+
+
+    void Start()
+    {
+
+    }
+    
+    void Update()
+    {
+
+    }
+}
