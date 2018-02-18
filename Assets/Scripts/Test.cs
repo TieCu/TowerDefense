bb21cc36025de9ac1ad63f08a3f3df307da3c6bb
Unholydeath <brbodily@student.neumont.edu>
2018-02-17 22:31:28 -0700

    Stuff with bullets

---
+++ Assets/Scripts/Test.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Test : MonoBehaviour {
+
+    private void OnTriggerEnter(Collider other)
+    {
+        Debug.Log("Somthing 3d entered range");
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        Debug.Log("Something 2d entered range");
+    }
+}
