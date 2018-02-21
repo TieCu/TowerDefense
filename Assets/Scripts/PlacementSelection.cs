72c8899ebffd68952607864d168b421694cfe15f
Zachery Anderson <zanderson@student.neumont.edu>
2018-02-21 13:25:09 -0700

    The hovering over is almost complete. The core implementation is there, but the check for if there is a tower is disabled for now and there is no placement of towers from it yet.

---
+++ Assets/Scripts/PlacementSelection.cs
@@ -0,0 +1,53 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlacementSelection : MonoBehaviour {
+
+	[SerializeField] Tower m_tower = null;
+	[SerializeField] LayerMask m_placeable;
+	private TilePiece m_currentTile = null;
+	private TilePiece m_priorTile = null;
+	private Color m_currTileColor;
+	private Color m_actualColor;
+
+	void Start () {
+		
+	}
+	
+	void Update () {
+	//	if(m_tower != null)
+		{
+			RaycastHit2D hit = Physics2D.Raycast(Camera.main.ScreenToWorldPoint(Input.mousePosition), Vector2.zero, 10, m_placeable);
+			if(hit.collider != null)
+			{
+
+				if (hit.collider.gameObject.GetComponent<TilePiece>().Occupied == false)
+				{
+					m_currentTile = hit.collider.gameObject.GetComponent<TilePiece>();
+					if (m_priorTile == m_currentTile)
+					{
+
+					} else
+					{
+						if(m_priorTile == null) m_priorTile = m_currentTile;
+						Renderer temp = hit.transform.gameObject.GetComponent<Renderer>();
+						m_actualColor = temp.material.color;
+						m_priorTile.gameObject.GetComponent<Renderer>().material.color = m_actualColor;
+						m_priorTile = m_currentTile;
+						m_currTileColor = Color.Lerp(m_actualColor, Color.green, 0.5f);
+						temp.material.color = m_currTileColor;
+						//Debug.Log("Target Position: " + hit.collider.gameObject.transform.position);
+					}
+				}
+			}
+		
+		}
+
+	}
+
+	public void SelectTower(Tower tower)
+	{
+		m_tower = tower;
+	}
+}
--- Assets/Scripts/World.cs
+++ Assets/Scripts/World.cs
@@ -1,6 +1,7 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using TMPro;
 
 public class World : Singleton<World>
 {
@@ -11,10 +12,16 @@ public class World : Singleton<World>
     [SerializeField] float m_coins;
 	[SerializeField] int m_maxPopulation;
 
+	[Header("Player Stats")]
+	[SerializeField] TextMeshProUGUI m_TxtLife = null;
+	[SerializeField] TextMeshProUGUI m_TxtMoney = null;
+
 	float m_timer = 0.0f;
 
 	void Start()
 	{
+		m_TxtMoney.text = "$" + m_coins.ToString();
+		m_TxtLife.text = m_health.ToString();
 
 	}
 
@@ -46,11 +53,13 @@ public class World : Singleton<World>
 	public void Invaded(float value)
 	{
 		m_health -= value;
+		m_TxtLife.text = m_health.ToString();
 	}
 
     public void AddToCoins(float newCoins)
     {
         m_coins += newCoins;
+		m_TxtMoney.text = "$" + m_coins.ToString();
     }
 
     public bool RemoveCoins(float cost)
@@ -62,7 +71,9 @@ public class World : Singleton<World>
             m_coins -= cost;
             wasSuccessful = true;
         }
+		m_TxtMoney.text = "$" + m_coins.ToString();
 
-        return wasSuccessful;       
+		return wasSuccessful;       
     }
+	
 }
