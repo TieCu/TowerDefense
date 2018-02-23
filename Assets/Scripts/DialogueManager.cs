6a15381477d46ff3714bf96a11536168bb6b16a0
Tien Cu <TCu@student.neumont.edu>
2018-02-23 04:44:12 -0700

    added more character sprites, edited dialogue manager to fit game

--- Assets/Scripts/DialogueManager.cs
+++ Assets/Scripts/DialogueManager.cs
@@ -6,15 +6,11 @@ using UnityEngine.UI;
 public class DialogueManager : MonoBehaviour
 {
 	[SerializeField] GameObject m_dialogBox;
-	[SerializeField] Button m_skip;
-	[SerializeField] Image m_playerPlacement;
-	[SerializeField] Image m_npcPlacement;
-	[SerializeField] Image m_optionalPlacement;
+	[SerializeField] CanvasGroup m_menu;
+	[SerializeField] Image m_portrait;
 	[SerializeField] Text m_namePlacement;
 	[SerializeField] Text m_dialogPlacement;
-	[SerializeField] Sprite[] m_playerSprites;
-	[SerializeField] Sprite[] m_npcSprites;
-	[SerializeField] Sprite[] m_optionalSprites;
+	[SerializeField] Sprite[] m_characterSprites;
 	[SerializeField] string[] m_names;
 	[SerializeField] string[] m_dialogs;
 	int m_currentLine = 0;
@@ -22,17 +18,21 @@ public class DialogueManager : MonoBehaviour
 
 	void Start()
 	{
+		if (m_dialogBox.activeInHierarchy)
+		{
+			m_menu.interactable = false;
+			Time.timeScale = 0.0f;
+		}
 	}
 
 	void Update()
 	{
-		if (m_talk && Input.GetButtonDown("Talk"))
+		if (m_talk && Input.GetMouseButtonDown(0))
 		{
 			m_dialogBox.SetActive(true);
-			Time.timeScale = 0.0f;
 		}
 
-		if (m_dialogBox.activeInHierarchy && Input.GetButtonDown("Talk"))
+		if (m_dialogBox.activeInHierarchy && Input.GetMouseButtonDown(0))
 		{
 			m_currentLine++;
 		}
@@ -40,23 +40,14 @@ public class DialogueManager : MonoBehaviour
 		if (m_currentLine >= m_dialogs.Length)
 		{
 			Time.timeScale = 1.0f;
+			m_menu.interactable = true;
 			m_dialogBox.SetActive(false);
 			m_currentLine = 0;
 		}
 
-		m_playerPlacement.sprite = m_playerSprites[m_currentLine];
-		if (m_npcPlacement != null)
-		{
-			m_npcPlacement.sprite = m_npcSprites[m_currentLine];
-		}
-		if (m_npcPlacement != null)
-		{
-			m_optionalPlacement.sprite = m_optionalSprites[m_currentLine];
-		}
+		m_portrait.sprite = m_characterSprites[m_currentLine];
 		m_namePlacement.text = m_names[m_currentLine];
 		m_dialogPlacement.text = m_dialogs[m_currentLine];
-
-		m_skip.onClick.AddListener(SkipDialog);
 	}
 
 	void OnTriggerEnter2D(Collider2D other)
@@ -67,12 +58,5 @@ public class DialogueManager : MonoBehaviour
 	void OnTriggerExit2D(Collider2D other)
 	{
 		m_talk = false;
-	}
-
-	void SkipDialog()
-	{
-		Time.timeScale = 1.0f;
-		m_dialogBox.SetActive(false);
-		m_currentLine = 0;
 	}
 }
