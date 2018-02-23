72802de99a86e4f6a8ec3ecfda533a04f6629b28
Unknown <brbodily@student.neumont.edu>
2018-02-23 11:42:24 -0700

    Changing tower's data structure

--- Assets/Scripts/TowerData.cs
+++ Assets/Scripts/TowerData.cs
@@ -19,29 +19,31 @@ public struct Status
     public float time;
 }
 
+public enum eTowerType
+{
+    BALISTA,
+    CANNON,
+    ICE,
+    LAVA,
+    MAGIC,
+    POISON
+}
+
 [CreateAssetMenu(fileName = "Data", menuName = "Data/Tower", order = 1)]
 
 public class TowerData : ScriptableObject
-{
-    public enum eTowerType
-    {
-        BALISTA,
-        CANNON,
-        ICE,
-        LAVA,
-        MAGIC,
-        POISON
-    }
-
+{   
     public float value;
     public float attackRadius;
     public float damage;
     public float attackRate;
     public eTowerType towerType;
     public Status towerStatus;
-    public float[] upgradeModifiers;
-    public float[] upgradeCosts;
-    public Sprite[] towers;
+    //public float[] upgradeModifiers;
+    //public float[] upgradeCosts;
+    //public Sprite[] towers;
+    public Sprite sprite;
+    public float upgradeCost;
     
     public Projectile projectile;
 }
